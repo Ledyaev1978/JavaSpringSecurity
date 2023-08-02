87b847179badc670f5f8f9ad2c25ee63f0c3d78f
Ledyaev1978 <chess_home@bk.ru>
2023-08-02 20:51:24 +0300

    2

---
+++ src/main/java/by/mysecurityspringgit/MySecuritySpringGitApplication.java
@@ -0,0 +1,13 @@
+package by.mysecurityspringgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecuritySpringGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySecuritySpringGitApplication.class, args);
+    }
+
+}
