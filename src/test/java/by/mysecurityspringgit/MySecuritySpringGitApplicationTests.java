11428cb7829a152b2564931b4bda1d047cff894a
Ledyaev1978 <chess_home@bk.ru>
2023-08-02 20:55:51 +0300

    4

---
+++ src/test/java/by/mysecurityspringgit/MySecuritySpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mysecurityspringgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecuritySpringGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
