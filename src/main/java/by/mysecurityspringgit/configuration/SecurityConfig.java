98690a45a7f6116da833adc5a61990e42c07623d
Ledyaev1978 <chess_home@bk.ru>
2023-08-05 18:58:31 +0300

    work 04/08/23 example without authentification provider

--- src/main/java/by/mysecurityspringgit/configuration/SecurityConfig.java
+++ src/main/java/by/mysecurityspringgit/configuration/SecurityConfig.java
@@ -1,23 +1,37 @@
 package by.mysecurityspringgit.configuration;
 
-import by.mysecurityspringgit.security.AuthProviderImpl;
+import by.mysecurityspringgit.services.UsersDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private  final AuthProviderImpl authProvider;
-
+    private final UsersDetailService usersDetailService;
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(UsersDetailService usersDetailService) {
+        this.usersDetailService = usersDetailService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    //private  final AuthProviderImpl authProvider;
+    /*@Autowired
+    public SecurityConfig(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
+    }*/
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersDetailService); //authenticationProvider(authProvider);
+
+    }
+
+     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
 
     }
 
